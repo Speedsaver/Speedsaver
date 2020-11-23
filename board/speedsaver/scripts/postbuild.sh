0e8b89f9898f66838f5c7ee824491a44afa0fb4b
Xogium <contact@xogium.me>
2020-11-23 16:21:33 -0500

    board/speedsaver: made path relative for fixed resolv.conf

--- board/speedsaver/scripts/postbuild.sh
+++ board/speedsaver/scripts/postbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 unlink "${TARGET_DIR}/etc/resolv.conf"
 
-ln -sf "${TARGET_DIR}/run/systemd/resolve/stub-resolv.conf" \
+ln -srf "${TARGET_DIR}"/run/systemd/resolve/stub-resolv.conf \
 	"${TARGET_DIR}/etc/resolv.conf"
