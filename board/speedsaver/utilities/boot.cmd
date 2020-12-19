cd68a2def28379693e8d518e41cd65ad220ec761
Xogium <contact@xogium.me>
2020-12-19 02:48:51 -0500

    board/speedsaver: add data partition to store logs.
    Fix boot script creation, passing correct rootfs to the kernel and loading files from mmc 0:2 instead of mmc 0.

--- board/speedsaver/utilities/boot.cmd
+++ board/speedsaver/utilities/boot.cmd
@@ -1,8 +1,8 @@
 setenv fdt_high ffffffff
 
-setenv bootargs console=ttyS0,115200 console=ttyGS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait quiet
+setenv bootargs console=ttyS0,115200 console=ttyGS0,115200 earlyprintk root=/dev/mmcblk0p3 rootwait quiet rw
 
-fatload mmc 0 $kernel_addr_r zImage
-fatload mmc 0 $fdt_addr_r sun8i-h2-plus-orangepi-zero.dtb
+fatload mmc 0:2 $kernel_addr_r zImage
+fatload mmc 0:2 $fdt_addr_r sun8i-h2-plus-orangepi-zero.dtb
 
 bootz $kernel_addr_r - $fdt_addr_r
