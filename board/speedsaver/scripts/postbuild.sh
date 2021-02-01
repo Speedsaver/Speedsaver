91dd04bf5c49a9a8f7b98351849958186bcc8813
Xogium <contact@xogium.me>
2021-02-01 05:59:33 -0500

    board/speedsaver: added version stamping information into the target.

--- board/speedsaver/scripts/postbuild.sh
+++ board/speedsaver/scripts/postbuild.sh
@@ -3,3 +3,8 @@ unlink "${TARGET_DIR}/etc/resolv.conf"
 
 ln -srf "${TARGET_DIR}"/run/systemd/resolve/stub-resolv.conf \
 	"${TARGET_DIR}/etc/resolv.conf"
+
+# copy version stamping information into the target
+
+cp "${BR2_EXTERNAL_SPEEDSAVER_PATH}"/utilities/version_stamp \
+	"${TARGET_DIR}"/etc/version_stamp
