9b37b01672fda062d32e26575229f2197de178b1
Xogium <contact@xogium.me>
2021-06-30 00:04:32 -0400

    Global: set the GIT_WORK_TREE environment variable.
    This turns out to be needed to point git to the work tree, as otherwise git describe always end up claiming the tree to be dirty should you not be precisely in the toplevel directory of your git repository.

--- external.mk
+++ external.mk
@@ -1,2 +1,2 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_SPEEDSAVER_PATH)/packages/*/*.mk))
-export SPEEDSAVER_VERSION=$(shell GIT_DIR=$(BR2_EXTERNAL_SPEEDSAVER_PATH)/.git git describe --broken --dirty)
+export SPEEDSAVER_VERSION=$(shell GIT_DIR=$(BR2_EXTERNAL_SPEEDSAVER_PATH)/.git GIT_WORK_TREE=$(BR2_EXTERNAL_SPEEDSAVER_PATH) git describe --broken --dirty)
