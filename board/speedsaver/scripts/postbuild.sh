1e668dd265e83c99f7ff2176b8b910b3c440171c
Xogium <contact@xogium.me>
2021-07-04 06:49:11 -0400

    board/speedsaver: make the generated version stamp file shorter so that it displays properly.

--- board/speedsaver/scripts/postbuild.sh
+++ board/speedsaver/scripts/postbuild.sh
@@ -6,5 +6,5 @@ ln -srf "${TARGET_DIR}"/run/systemd/resolve/stub-resolv.conf \
 
 # create version stamping information into the target
 
-echo "Speedsaver version "${SPEEDSAVER_VERSION}" on buildroot "${BR2_VERSION}"" \
+echo "V "${SPEEDSAVER_VERSION}" on BR "${BR2_VERSION}"" \
 	> "${TARGET_DIR}"/etc/version_stamp
