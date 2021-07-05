3d1cf789b420203847bfd3ef791c362bb549386f
Xogium <contact@xogium.me>
2021-07-05 02:08:05 -0400

    packages/navit: bump to commit 6c34f7ab15dbf60a9b483dfc578ae9063bdf872b.

--- packages/navit/navit.mk
+++ packages/navit/navit.mk
@@ -26,7 +26,7 @@ NAVIT_CONF_OPTS += -DDISABLE_QT=1 -Dgraphics/qt5=0 -Dvehicle/qt5=0
 endif
 
 ifeq ($(BR2_PACKAGE_NAVIT_SPEEDSAVER),y)
-NAVIT_VERSION = 73e55645df103a812410fd50fa21dc4db29d8b3b
+NAVIT_VERSION = 6c34f7ab15dbf60a9b483dfc578ae9063bdf872b
 NAVIT_SITE = git://github.com/speedsaver/navit
 NAVIT_DEPENDENCIES += arduipi-oled libglib2
 NAVIT_CONF_OPTS += -Dspeech/cmdline=0 -Dmap/textfile=0 -Dgui/internal=0 -DUSE_NATIVE_LANGUAGE_SUPPORT=0
