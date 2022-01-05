3c2aaf7ef244f8ff93bb24915d5bae36fcf2091d
Xogium <contact@xogium.me>
2022-01-05 08:58:10 +0100

    board/speedsaver: provide some more environment variables to the post-image script needed for rauc.

--- board/speedsaver/scripts/postimage.sh
+++ board/speedsaver/scripts/postimage.sh
@@ -7,8 +7,12 @@ cd "${BINARIES_DIR}"
 export GENIMAGE_TMP=${BR2_EXTERNAL_SPEEDSAVER_PATH}/board/speedsaver/utilities/genimage.tmp
 GENIMAGE_CFG="${BR2_EXTERNAL_SPEEDSAVER_PATH}/board/speedsaver/utilities/genimage.cfg"
 export GENIMAGE_BUILD_ID=$(echo "${SPEEDSAVER_VERSION}")
+export GENIMAGE_HOST=$(hostname)
+export GENIMAGE_USER=$(whoami)
 
 envsubst \
-\$GENIMAGE_BUILD_ID \
+\$GENIMAGE_BUILD_ID,\
+\$GENIMAGE_HOST,\
+\$GENIMAGE_USER \
 < ${GENIMAGE_TMP} \
 > ${GENIMAGE_CFG}
