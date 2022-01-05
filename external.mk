df9cf46e10ea589153f9b313908d9e663d5ca114
Xogium <contact@xogium.me>
2022-01-05 07:56:51 +0100

    Global: added infrastructure necessary for rauc.

--- external.mk
+++ external.mk
@@ -1,2 +1,6 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_SPEEDSAVER_PATH)/packages/*/*.mk))
 export SPEEDSAVER_VERSION=$(shell GIT_DIR=$(BR2_EXTERNAL_SPEEDSAVER_PATH)/.git GIT_WORK_TREE=$(BR2_EXTERNAL_SPEEDSAVER_PATH) git describe --broken --dirty)
+
+rauc-fixups:
+	$(BR2_EXTERNAL_SPEEDSAVER_PATH)/scripts/rauc_fixups.sh
+world: rauc-fixups
---
+++ scripts/rauc_fixups.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+. "${BR2_EXTERNAL_SPEEDSAVER_PATH}"/scripts/certs.sh
+
+# creating a clean directory to store copies of files in
+
+CERTS_DIR="${BR2_EXTERNAL_SPEEDSAVER_PATH}"/certs
+test -d "${CERTS_DIR}" && rm -r "${CERTS_DIR}"
+mkdir -p "${CERTS_DIR}"
+
+# lets copy the appropriate files to use later with genimage as well as in rootfs_postbuild.sh
+
+cp "${RAUC_CERTIFICATE_AUTHORITY}" "${CERTS_DIR}"/keyring.pem
+cp "${RAUC_PRIVATE_KEY}" "${CERTS_DIR}"/signing.key
+cp "${RAUC_PUBLIC_KEY}" "${CERTS_DIR}"/signing.crt
